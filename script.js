38b737d9affa74a56e06e8fe8e84248eac92a9ca
Alex Pockrus <xpockrus@gmail.com>
2023-06-08 16:44:45 -0700

    Trimmed if/else statements, time for game function

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
-/*let computerSelection;
-let playerSelection;*/
+let computerSelection;
+let playerSelection
 
 const possibleComputerChoices = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice() {
-    return possibleComputerChoices[~~(Math.random() * possibleComputerChoices.length)];
-}
-function getPlayerChoice() {
-    return prompt("Rock, paper, scissors? Bruv?");
+    
 }
 
+let playerScore = 0
+let computerScore = 0
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === "rock" && computerSelection === "Scissors") {
-        return "Aye bruv you won! Rock beats Scissors!"
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "Paper") {
-        return "Sorry mate, you don lost this one. Rock losers hard to Paper!"
-    } else if (playerSelection.toLowerCase() === "rock" && computerSelection === "Rock") {
-        return "Oi! You tied! Rock vs Rock! Oil up them fingers and go again!" 
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Scissors") {
-        return "Scissor me timbers, you tied! Have a go again!"
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Paper") {
-        return "Snip Snip Snip! You cut that poor computers paper roight in havv den!"
-    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "Rock") {
-        return "Oof, those scissors got busted. Better luck next time mate." 
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Scissors") {
-        return "Oi you alright? Your paper just got nixed right in the knickers."
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Paper") {
-        return "Probably the most boring tie you can imagine. Paper vs Paper."
-    } else if (playerSelection.toLowerCase() === "paper" && computerSelection === "Rock") {
-        return "Aye look at you, covering that rock with your paper, you don good kid."
+    computerSelection = getComputerChoice();
+    function getComputerChoice() {
+        return possibleComputerChoices[~~(Math.random() * possibleComputerChoices.length)];
+    }
+    console.log(getComputerChoice());
+    playerSelection = getPlayerChoice().toLowerCase();
+    function getPlayerChoice() {
+        return prompt("Rock, paper, scissors? Bruv?");
+    }
+    console.log(playerSelection);
+
+    if ((playerSelection === "rock" && computerSelection === "Scissors") ||
+    (playerSelection === "scissors" && computerSelection === "Paper") ||
+    (playerSelection === "paper" && computerSelection === "Rock")) {
+        return (++playerScore)
+    } else if ((playerSelection === "rock" && computerSelection === "Paper") ||
+    (playerSelection === "scissors" && computerSelection === "Rock")
+    (playerSelection === "paper" && computerSelection === "Scissors")) {
+        return (++computerScore)
+    } else if (playerSelection == computerSelection.toLowerCase()) {
+        return ("It's bloody tie ya wanker");
     } else {
         return "Oi what the frik? can't you shpell?"
     }
 }
+function game()
 
-let playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
 console.log(playRound(playerSelection, computerSelection));
